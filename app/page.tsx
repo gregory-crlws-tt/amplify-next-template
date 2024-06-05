8a5781fdad524acc2ca0e9a0812afa88a72c8fc0
gregory-curlewis-geollect <greg.curlewis@triremetrading.com>
2024-06-05 10:42:57 +0100

    added authenticator

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,15 @@ import type { Schema } from "@/amplify/data/resource";
 import "./../app/app.css";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
+
+import { Authenticator } from '@aws-amplify/ui-react'
 import "@aws-amplify/ui-react/styles.css";
 
 Amplify.configure(outputs);
+
+function deleteTodo(id: string) {
+  client.models.Todo.delete({ id });
+}
 
 const client = generateClient<Schema>();
 
@@ -32,21 +38,33 @@ export default function App() {
   }
 
   return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
-          Review next steps of this tutorial.
-        </a>
-      </div>
-    </main>
+
+
+    <Authenticator>
+      {({ signOut, user }) => (
+
+        <main>
+          <h1>My todos</h1>
+          <button onClick={createTodo}>+ new</button>
+          <ul>
+            {todos.map((todo) => (
+              <li
+                onClick={() => deleteTodo(todo.id)}
+                key={todo.id}>{todo.content}</li>
+            ))}
+          </ul>
+          <div>
+            🥳 App successfully hosted. Try creating a new todo.
+            <br />
+            <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
+              Review next steps of this tutorial.
+            </a>
+          </div>
+
+          <button onClick={signOut}>Sign out</button>
+
+        </main>
+      )}
+    </Authenticator>
   );
 }
